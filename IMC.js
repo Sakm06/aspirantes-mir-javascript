7fd48368da6991d3db1b270eb5f5ee9b64926562
Sakm06 <Sakmsta06@gmail.com>
2023-05-11 22:20:09 -0600

    IMC fix v1

--- IMC.js
+++ IMC.js
@@ -1,6 +1,9 @@
-// escribe la función bmi acá
+let peso=(105);
+let altura=(1.7);
+let IMC=(peso/(altura*altura))
+function bmi(IMC) {
+    console.log("Su IMC es: "+IMC);
+}
+bmi(IMC);
 
-// código de prueba
-console.log(bmi(65, 1.8)) // 20.061728395061728
-console.log(bmi(72, 1.6)) // 28.124999999999993
-console.log(bmi(52, 1.75)) //  16.979591836734695
+
