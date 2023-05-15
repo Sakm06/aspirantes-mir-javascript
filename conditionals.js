17a8264c162af9f5d3b42d80439096afaea00163
Sakm06 <Sakmsta06@gmail.com>
2023-05-15 16:12:58 -0600

    Solucion a los ejercicios de js

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+
+let numero
+let color
+function calcularColor(numero) {
+
+    if(numero===1){
+     
+      color = "Negro"};
+
+ if(numero===2){color ="blanco"} ;
+    
+ if(numero===3){color ="rojo"};
+     
+ if (numero===4)
+    {color="verde"};    
+  
+    return "El color es " + color;
+  }
+console.log (calcularColor(2))
