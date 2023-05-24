23fdf973e785579d3c3d238ac697650ad34c24d6
Sakm06 <Sakmsta06@gmail.com>
2023-05-23 20:28:57 -0600

    Joinadded

---
+++ Js-2/Join.js
@@ -0,0 +1,9 @@
+let array=["Hola ", "Como ", "Estas ", "?"]
+let hola=array[0]
+let como=array[1]
+let estas=array[2]
+let pregunta=array[3]
+function join(){
+    console.log(hola+como+estas+pregunta)
+}
+join()
